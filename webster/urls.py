1c7aa414f040bf8a19c2c015ed670339f9cd3641
Oleksandr Zelenchuk <sasha.zelenchuk@gmail.com>
2021-08-26 21:27:27 +0300

    static_files

--- webster/urls.py
+++ webster/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from crm import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.first_page),
     path('thanks/', views.thanks_page, name = 'thanks_page'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
