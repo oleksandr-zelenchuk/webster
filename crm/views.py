14de15940c303101ce22fc0a22ca7ba303b69591
Oleksandr Zelenchuk <sasha.zelenchuk@gmail.com>
2021-08-26 13:26:25 +0300

    get method

--- crm/views.py
+++ crm/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Order
+
+
+def first_page(request):
+    object_list = Order.objects.all()
+    return render(request, './index.html', {'object_list': object_list})
+
+
+def thanks_page(request):
+    name = request.GET['name']
+    phone = request.GET['phone']
+    element = Order(order_name=name, order_phone=phone)
+    element.save()
+    return render(request, './thanks_page.html', {'name': name,
+                                                  'phone': phone})
 
 # Create your views here.
